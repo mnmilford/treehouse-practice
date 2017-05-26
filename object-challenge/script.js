5557122dece88baf179d58ff6b7dd431d97814bc
Michael Milford <mnmilford@gmail.com>
2017-05-25 20:48:02 -0500

    Initial commit - create starter html & js file for object challenge

---
+++ object-challenge/script.js
@@ -0,0 +1,32 @@
+var students = [
+  {
+    name: "Michael Milford",
+    track: "Front End Development",
+    achievements: 50,
+    points: 4220
+  },
+  {
+    name:
+    track:
+    achievements:
+    points:
+  },
+  {
+    name:
+    track:
+    achievements:
+    points:
+  },
+  {
+    name:
+    track:
+    achievements:
+    points:
+  },
+  {
+    name:
+    track:
+    achievements:
+    points:
+  }
+];
