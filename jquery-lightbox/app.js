d68c3312ed9e0df8e7d2f15a26d6b94aa918a31b
Michael Milford <mnmilford@gmail.com>
2017-06-03 18:58:24 -0500

    Append fullsize img to overlay before fading in overlay

--- jquery-lightbox/app.js
+++ jquery-lightbox/app.js
@@ -3,8 +3,8 @@ $('.overlay').hide();
 
 $("#imageGallery img").click(function(event){
   let $imgSrc = $(this).clone().attr("src");
+  $('.overlay').append("<img class='fullsize' src='" + $imgSrc + "'</img>");
   $('.overlay').fadeIn("fast", function(){
-    $(this).append("<img  class='fullsize' src='" + $imgSrc + "'</img>");
     // If user clicks on overlay it will fade out
     $(this).click(function(){
       $(this).fadeOut("fast", function(){
