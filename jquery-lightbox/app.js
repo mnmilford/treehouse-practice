fa70b049f4c438a284441ae2c35185a1100b3d8a
Michael Milford <mnmilford@gmail.com>
2017-06-04 16:49:29 -0500

    Add script for caption on img when selected

--- jquery-lightbox/app.js
+++ jquery-lightbox/app.js
@@ -1,10 +1,21 @@
-let $overlay = $('<div class="overlay"></div>');
+let $overlay = $("<div class='overlay'></div>");
 let $img = $("<img class='fullsize'></img>");
+let $caption = $("<div class='caption'></div>");
 
 $("#imageGallery img").click(function(){
+  // Figure out the src attribute of the image that's clicked
   let $imgSrc = $(this).attr("src");
+  // Assign the src to the $img variable
   $img.attr("src", $imgSrc);
+  // Add the $img to the overlay
   $overlay.append($img);
+  // Grab the alt tag of the clicked image
+  let $imgAlt = $(this).attr("alt");
+  // Assign the alt as text content of $caption
+  $caption.text($imgAlt);
+  // Add caption to the overlay
+  $overlay.append($caption);
+  // Finally, add the overlay to the body
   $("body").append($overlay);
   $overlay.animate({opacity: 1}, 200);
   $overlay.click(function(){
